bbbed45fb955589b8bc0003029bf27d7f4fd02c7
Atharva Yadav <atharvayadav11@gmail.com>
2025-09-28 19:58:00 +0530

    Add frontend socket - om

---
+++ frontend/src/app/services/socket.services.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { io, Socket } from 'socket.io-client';
+
+@Injectable({ providedIn: 'root' })
+export class SocketService {
+  private socket: Socket;
+
+  constructor() {
+    this.socket = io('http://localhost:3000');
+  }
+
+  emit(event: string, data?: any){
+    this.socket.emit(event, data);
+  }
+
+  on<T>(event: string): Observable<T>{
+    return new Observable(observer => {
+        this.socket.on(event, (data : T) => observer.next(data));
+    })
+  }
+  
+}
